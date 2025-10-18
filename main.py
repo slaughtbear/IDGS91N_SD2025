de1a8203fa4f27ad6d3a9a88a981e546dabff720
Adrian Mata <cnevarez751@gmail.com>
2025-10-17 20:27:54 -0600

    Cambios de Adrián

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 if __name__ == "__main__":
     print("Este es un cambio")
     print("Este es otro cambio para causar un conflicto...")
+
+    # Estos son los cambios que se le van a realizar al archivo para hacer pull request
+
+
+
+    print("Este es un cambio adicional para causar otro conflicto...")
+    print("Este cambio es para probar el pull request")
+    print("Cambio final para probar pull request")
+    print("Ultimo cambio para probar pull request")
+    print("Cambio extra para probar pull request")
+
+
+
+
+    
